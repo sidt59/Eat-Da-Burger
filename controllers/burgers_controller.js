eb65289659df6209585712cf18a00aa4f8eb4600
STS 59 <notippin@gmail.com>
2019-04-11 18:27:20 -0500

    logic file created. damn thing wont render

--- controllers/burgers_controller.js
+++ controllers/burgers_controller.js
@@ -17,23 +17,23 @@ router.get("/", function(req, res) {
   });
   
   router.post("/api/burgers", function(req, res) {
-    /* cat.create([
-      "name", "sleepy"
+    burger.create([
+      "burger_name"
     ], [
-      req.body.name, req.body.sleepy
+      req.body.burger_name
     ], function(result) {
       // Send back the ID of the new quote
       res.json({ id: result.insertId });
-    }); */
+    });
   });
   
-  router.put("/api/cats/:id", function(req, res) {
-    /* var condition = "id = " + req.params.id;
+  router.put("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
   
     console.log("condition", condition);
   
-    cat.update({
-      sleepy: req.body.sleepy
+    burger.update({
+      devoured: req.body.devoured
     }, condition, function(result) {
       if (result.changedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
@@ -41,20 +41,20 @@ router.get("/", function(req, res) {
       } else {
         res.status(200).end();
       }
-    }); */
+    });
   });
   
-  router.delete("/api/cats/:id", function(req, res) {
-    /* var condition = "id = " + req.params.id;
+  router.delete("/api/burgers/:id", function(req, res) {
+    var condition = "id = " + req.params.id;
   
-    cat.delete(condition, function(result) {
+    burger.delete(condition, function(result) {
       if (result.affectedRows == 0) {
         // If no rows were changed, then the ID must not exist, so 404
         return res.status(404).end();
       } else {
         res.status(200).end();
       }
-    }); */
+    });
   });
   
   // Export routes for server.js to use.

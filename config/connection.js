adec790093131fbee24dc62eb2c086d6c79c1f41
STS 59 <notippin@gmail.com>
2019-04-12 23:16:00 -0500

    whyyyyyyyyyy goddddddddd

--- config/connection.js
+++ config/connection.js
@@ -14,7 +14,13 @@ let connection;
        });
    };
 
-connection.connect();
+   connection.connect(function(err) {
+    if (err) {
+        console.error("error connecting: " + err.stack);
+        return;
+    }
+    console.log("connected as id " + connection.threadId);
+  });
 //export
 module.exports = connection
 
